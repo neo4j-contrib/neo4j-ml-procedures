d56491d8b294cfd25256c8c7002f9fda22584a9e
Lauren Shin <laurenshin@Lauren-Shin-Macbook-Pro.local>
2018-05-30 14:56:11 -0700

    cleaned up test

--- src/test/java/regression/LRTest.java
+++ src/test/java/regression/LRTest.java
@@ -1,6 +1,7 @@
 package regression;
 
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 
 import org.junit.After;
@@ -34,6 +35,22 @@ public class LRTest {
         db.shutdown();
     }
 
+    private void exhaust(Iterator r) {
+        while(r.hasNext()) r.next();
+    }
+
+    private void check(Result result, Map<Double, Double> expected) {
+        while (result.hasNext()) {
+            Map<String, Object> actual = result.next();
+
+            double time = (double) actual.get("time");
+            double expectedPrediction = expected.get(time);
+            double actualPrediction = (double) actual.get("predictedProgress");
+
+            assertThat( actualPrediction, equalTo( expectedPrediction ) );
+        }
+    }
+
     @Test
     public void regression() throws Exception {
         //create known relationships for times 1, 2, 3
@@ -52,7 +69,7 @@ public class LRTest {
         Result r = db.execute("MATCH () - [r:WORKS_FOR] -> () WHERE exists(r.time) AND exists(r.progress) CALL " +
                 "regression.linear.add('work and progress', r.time, r.progress) RETURN r");
         //these rows are computed lazily so we must iterate through all rows to ensure all data points are added to the model
-        while(r.hasNext()) r.next();
+        exhaust(r);
 
         //check that the correct info is stored in the model (should contain 3 data points)
         Map<String, Object> info = db.execute("CALL regression.linear.info('work and progress') YIELD model, state, N " +
@@ -62,8 +79,9 @@ public class LRTest {
         assertThat(info.get("N"), equalTo(3.0));
 
         //store predictions
-        db.execute("MATCH () - [r:WORKS_FOR] -> () WHERE exists(r.time) AND NOT exists(r.progress) SET " +
-                "r.predictedProgress = regression.linear.predict('work and progress', r.time)");
+        String storePredictions = "MATCH (:Node)-[r:WORKS_FOR]->(:Node) WHERE exists(r.time) AND NOT exists(r.progress) " +
+                "SET r.predictedProgress = regression.linear.predict('work and progress', r.time)";
+        db.execute(storePredictions);
 
         //check that predictions are correct
 
@@ -80,21 +98,13 @@ public class LRTest {
 
         Result result = db.execute(gatherPredictedValues);
 
-        while (result.hasNext()) {
-            Map<String, Object> actual = result.next();
-
-            double time = (double) actual.get("time");
-            double expectedPrediction = expected.get(time);
-            double actualPrediction = (double) actual.get("predictedProgress");
-
-            assertThat(actualPrediction, equalTo(expectedPrediction));
-        }
+        check(result, expected);
 
         //serialize the model
         Map<String, Object> serial = db.execute("RETURN regression.linear.serialize('work and progress') as data").next();
         Object data = serial.get("data");
 
-        //check that the model returns same predictions as the model stored in the procedure
+        //check that the byte[] model returns same predictions as the model stored in the procedure
         SimpleRegression storedR = (SimpleRegression) LR.convertFromBytes((byte[]) data);
         assertThat(storedR.predict(4.0), equalTo(expected.get(4.0)));
         assertThat(storedR.predict(5.0), equalTo(expected.get(5.0)));
@@ -108,7 +118,7 @@ public class LRTest {
         //remove data from relationship between nodes 1 and 2
         r = db.execute("MATCH (:Node {id:1})-[r:WORKS_FOR]->(:Node {id:2}) CALL regression.linear.remove('work " +
                 "and progress', r.time, r.progress) return r");
-        while (r.hasNext()) r.next();
+        exhaust(r);
 
         //create a new relationship between nodes 7 and 8
         db.execute("MATCH (n7:Node {id:7}) MERGE (n7)-[:WORKS_FOR {time:6.0, progress:5.870}]->(:Node {id:8})");
@@ -117,54 +127,36 @@ public class LRTest {
         r = db.execute("MATCH (:Node {id:7})-[r:WORKS_FOR]->(:Node {id:8}) CALL regression.linear.add('work " +
                 "and progress', r.time, r.progress) RETURN r.time");
         //again must iterate through rows
-        while (r.hasNext()) r.next();
+        exhaust(r);
 
         //map new model on all relationships with unknown progress
-        db.execute("MATCH (:Node)-[r:WORKS_FOR]->(:Node) WHERE exists(r.time) AND NOT exists(r.progress) " +
-                "SET r.predictedProgress = regression.linear.predict('work and progress', r.time)");
+        db.execute(storePredictions);
 
         //replicate the creation and updates of the model
         R.removeData(1.0, 1.345);
         R.addData(6.0, 5.870);
-
         expected.put(4.0, R.predict(4.0));
         expected.put(5.0, R.predict(5.0));
 
         //make sure predicted values are correct
         result = db.execute(gatherPredictedValues);
-        while (result.hasNext()) {
-            Map<String, Object> actual = result.next();
+        check(result, expected);
 
-            double time = (double) actual.get("time");
-            double expectedPrediction = expected.get(time);
-            double actualPrediction = (double) actual.get("predictedProgress");
-
-            assertThat( actualPrediction, equalTo( expectedPrediction ) );
-        }
-
+        //test addM procedure for adding multiple data points
         double[] points = {7.0, 8.0};
         double[] observed = {6.900, 9.234};
         params.put("points", points);
         params.put("observed", observed);
 
         db.execute("CALL regression.linear.addM('work and progress', $points, $observed)", params);
-        db.execute("MATCH (:Node)-[r:WORKS_FOR]->(:Node) WHERE exists(r.time) AND NOT exists(r.progress) " +
-                "SET r.predictedProgress = regression.linear.predict('work and progress', r.time)");
+        db.execute(storePredictions);
         R.addData(7.0, 6.900);
         R.addData(8.0, 9.234);
         expected.put(4.0, R.predict(4.0));
         expected.put(5.0, R.predict(5.0));
         result = db.execute(gatherPredictedValues);
 
-        while (result.hasNext()) {
-            Map<String, Object> actual = result.next();
-
-            double time = (double) actual.get("time");
-            double expectedPrediction = expected.get(time);
-            double actualPrediction = (double) actual.get("predictedProgress");
-
-            assertThat( actualPrediction, equalTo( expectedPrediction ) );
-        }
+        check(result, expected);
 
         db.execute("CALL regression.linear.delete('work and progress')").close();
 
